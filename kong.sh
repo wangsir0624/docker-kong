e3c2498e00c4d8b11c5b84b20095a08203a48cfd
wangjian <1636801376@qq.com>
2018-03-05 18:51:28 +0800

    fix

---
+++ kong.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+#将工作目录切换到脚本所在目录
+basepath=$(cd `dirname $0`; pwd)
+cd ${basepath}
+
+#获取服务名称
+servicename=${basepath##*/}
+
+printHelp() {
+    cat <<EOF
+this
+is
+help
+EOF
+}
+
+checkDatabaseType() {
+    if [ "$1" != 'postgres' -a "$1" != 'cassandra' -a "$1" != 'external' ]
+    then
+        printHelp
+        exit 1
+    fi
+}
+
+checkDatabaseType $2
+if [ "$1" == 'start' ]
+then
+    while true
+    do
+        `curl -s http://127.0.0.1:8000 > /dev/null`
+        kongStatus=$?
+        `curl -s http://127.0.0.1:1337 > /dev/null`
+        kongaStatus=$?
+        if [ "${kongStatus}" == "0" -a "${kongaStatus}" == "0" ]
+        then
+            break
+        fi
+
+        case $2 in
+            postgres)
+            `docker-compose -f docker-compose.yml -f docker-compose.postgres.yml up -d ${@:3}`
+            ;;
+            cassandra)
+            `docker-compose -f docker-compose.yml -f docker-compose.cassandra.yml up -d ${@:3}`
+            ;;
+            external)
+            `docker-compose -f docker-compose.yml up -d ${@:3}`
+            ;;
+        esac
+    done
+elif [ "$1" == 'stop' ]
+then
+    case $2 in
+        postgres)
+        `docker-compose -f docker-compose.yml -f docker-compose.postgres.yml down ${@:3}`
+        ;;
+        cassandra)
+        `docker-compose -f docker-compose.yml -f docker-compose.cassandra.yml down ${@:3}`
+        ;;
+        external)
+        `docker-compose -f docker-compose.yml down ${@:3}`
+        ;;
+    esac
+else
+    printHelp
+fi
